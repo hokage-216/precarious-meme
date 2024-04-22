ddbada4c1b0f9cbc6db69a749eef068d1b9ba83b
LeVente Berry <berrylevente@gmail.com>
2024-04-22 14:52:14 -0400

    updating webpack config file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,9 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
   return {
@@ -18,13 +16,58 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generates an HTML file from a template
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html'
+      }),
+
+      // Extract CSS to files
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      }),
+
+      // Creates a service worker injecting the precache manifest
+      new InjectManifest({
+        swSrc: './src/sw.js', // path to the source service worker file
+        swDest: 'service-worker.js', // output service worker file
+        exclude: [/\.map$/, /^manifest.*\.js(?:on)?$/]
+      }),
+
+      // Generates a 'manifest.json'
+      new WebpackPwaManifest({
+        name: 'Text Editor PWA',
+        short_name: 'TextEditor',
+        description: 'An offline capable text editor!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
 };
+
